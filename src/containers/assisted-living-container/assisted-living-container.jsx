df1ab0498584880a9a694bb82fcd119e6376478b
Aaron Sisler <aaron.sisler@eandbsolutions.com>
2019-12-01 19:41:59 -0500

    Adding in the assisted living content.  Need to tweak for wording

--- src/containers/assisted-living-container/assisted-living-container.jsx
+++ src/containers/assisted-living-container/assisted-living-container.jsx
@@ -1,15 +1,34 @@
 import React from "react";
+import assistedLivingServices from "../../content/assisted-living-services";
 
 import "./assisted-living-container.scss";
 
 const AssistedLivingContainer = () => (
   <div className="assisted-living-container">
-    At Brookdale we offer much more than a place to stay — we offer a place to
-    live well. Our residents share some common values: to be recognized as
-    individuals, to lead lives with a sense of purpose, and to maintain
-    connections with people and interests that are meaningful to them. Because
-    we’re passionate about enabling those values, we are constantly innovating
-    new and better ways for enriching the lives of our senior living residents.
+    <div className="assisted-living-container__splash-image" />
+    <div className="assisted-living-container__content">
+      <h1 className="assisted-living-container__title">
+        Our services make the difference
+      </h1>
+      <p>
+        At Waterbrooke, we offer much more than a place to stay. We offer a
+        place to live well. Our residents share some common values: to be
+        recognized as individuals, to lead lives with a sense of purpose, and to
+        maintain connections with people and interests that are meaningful to
+        them.
+      </p>
+      <hr />
+      <h2 className="assisted-living-container__list-title">
+        Assisted Living services
+      </h2>
+      <ul className="assisted-living-container__list">
+        {assistedLivingServices.map((service, index) => (
+          <li key={index} className="assisted-living-container__list-item">
+            {service}
+          </li>
+        ))}
+      </ul>
+    </div>
   </div>
 );
 
