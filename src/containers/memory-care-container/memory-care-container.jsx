9e02985d5633ea7f1d44cbaec7e410cd4c3c2ec7
Aaron Sisler <aaron.sisler@eandbsolutions.com>
2019-12-01 19:50:13 -0500

    Adding in some text for memory care

--- src/containers/memory-care-container/memory-care-container.jsx
+++ src/containers/memory-care-container/memory-care-container.jsx
@@ -3,7 +3,20 @@ import React from "react";
 import "./memory-care-container.scss";
 
 const MemoryCareContainer = () => (
-  <div className="memory-care-container">MemoryCareContainer</div>
+  <div className="memory-care-container">
+    <div className="memory-care-container__splash-image" />
+    <div className="memory-care-container__content">
+      <h1 className="memory-care-container__title">What is memory care?</h1>
+      <p>
+        Our memory care program features a person-centered approach that’s
+        designed to help your loved one feel a sense of belonging and purpose,
+        while still preserving their identity and individuality. Featuring a
+        safe and secure environment, seniors are able to participate in our
+        daily path of engagement, which encourages their current skills through
+        research-based activities.
+      </p>
+    </div>
+  </div>
 );
 
 export default MemoryCareContainer;
