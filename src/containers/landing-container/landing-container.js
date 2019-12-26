6f6d8ee9e7144037836505fec4e7a3c8e1748a29
Aaron Sisler <aaron.sisler@eandbsolutions.com>
2019-12-26 12:11:08 -0500

    Adding in the landing page

--- src/containers/landing-container/landing-container.js
+++ src/containers/landing-container/landing-container.js
@@ -1,11 +1,47 @@
 import React from "react";
-import Landing from "../../components/landing";
+import Image from "../../atoms/image";
+import Hyperlink from "../../atoms/hyperlink";
+import { IMAGES_CDN } from "../../config";
 
 import "./landing-container.scss";
 
+const splashImage = {
+  altText: "Flowers on the front outdoor porch",
+  desktopImgSrc: `${IMAGES_CDN}/landing-container__splash--mobile.png`,
+  mobileImgSrc: `${IMAGES_CDN}/landing-container__splash--mobile.png`
+};
+
 const LandingContainer = () => (
   <div className="landing-container">
-    <Landing />
+    <div className="landing-container__splash">
+      <Image className="landing-container__splash-image" {...splashImage} />
+    </div>
+    <div className="landing-container__content">
+      <h1 className="landing-container__content-title">
+        Life is better at Waterbrooke
+      </h1>
+      <p>
+        Located in the beautiful and historic “Harbor of Hospitality”,
+        Waterbrooke of Elizabeth City is a licensed assisted living community.
+        We are conveniently located next to the Sentara Albemarle Hospital with
+        access to many local healthcare providers.
+      </p>
+      <p>
+        You will find days with fewer worries, more options, and a community
+        that feels just like home. Find a sense of belonging in our community of
+        friends by&nbsp;
+        <Hyperlink href="/daily-life" title="learning more here." />
+      </p>
+      <h2 className="landing-container__content-sec-title">
+        Not sure where to begin?
+      </h2>
+      <p>
+        Sometimes it is hard to know what questions to even begin asking when it
+        comes to the care of your loved ones. We are here to help you understand
+        what we can provide and recommend&nbsp;
+        <Hyperlink href="/why-assisted-living" title="starting here." />
+      </p>
+    </div>
   </div>
 );
 
