48bb61816c6cd7e9a4b316d945ec2b22f3b9d113
Aaron Sisler <aaron.sisler@eandbsolutions.com>
2020-01-12 15:12:27 -0500

    Adding in the download component and setting up the markup structure of careers container

---
+++ src/components/application-download/application-download.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import "./application-download.scss";
+
+const ApplicationDownload = () => (
+  <div className="application-download">ApplicationDownload</div>
+);
+
+export default ApplicationDownload;
--- src/containers/careers-container/careers-container.jsx
+++ src/containers/careers-container/careers-container.jsx
@@ -1,11 +1,16 @@
 import React from "react";
+import ApplicationDownload from "../../components/application-download";
 import ApplicationSubmissionForm from "../../components/application-submission-form";
 
 import "./careers-container.scss";
 
 const CareersContainer = () => (
   <div className="careers-container">
-    <ApplicationSubmissionForm />
+    <div className="careers-container__header">Careers Header</div>
+    <div className="careers-container__content">
+      <ApplicationDownload />
+      <ApplicationSubmissionForm />
+    </div>
   </div>
 );
 
