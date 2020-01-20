4fe09e0cfef44f6d0b8200cb294f00cf9a78e307
Aaron Sisler <aaron.sisler@eandbsolutions.com>
2020-01-20 11:25:35 -0500

    Adding in the activities list and refactoring the CSS for layout

--- src/content/activities.js
+++ src/content/activities.js
@@ -18,4 +18,26 @@ const eventImages = [
   }
 ];
 
-export { eventImages };
+const activities = [
+  "Monthly Resident Outing (Weather Permitting)",
+  "SPCA Pet Visit",
+  "Monthly Birthday Party",
+  "Bingo",
+  "Exercise Activities",
+  "Kingdom Hall Bible Study",
+  "Devotion",
+  "Resident Council Meeting Monthly",
+  "Men's Club",
+  "One to One Room Visits",
+  "Memory Stimulation",
+  "Front & Back Porch Activities (Weather Permitting)",
+  "Cooking Class",
+  "Arts & Crafts",
+  "Wade Nevans & Friends Gospel Group",
+  "Visit from Elizabeth City / Pasquotank County Senior Center",
+  "Visit from Perquimans County Senior Center",
+  "Annual Senior Prom (Summer)",
+  "Annual Christmas Party (December)"
+];
+
+export { activities, eventImages };
