5995804d86fa2a106e215d74c082a215ce00ad3b
Aaron Sisler <aaron.sisler@eandbsolutions.com>
2020-01-20 11:40:28 -0500

    Switching the CDN path for images

--- src/content/activities.js
+++ src/content/activities.js
@@ -3,18 +3,18 @@ import { CDN_URL } from "../config";
 const eventImages = [
   {
     altText: "Disco party for residents",
-    desktopImgSrc: `${CDN_URL}/activities-container__disco--desktop.jpg`,
-    mobileImgSrc: `${CDN_URL}/activities-container__disco--desktop.jpg`
+    desktopImgSrc: `${CDN_URL}/activities/activities-container__disco--desktop.jpg`,
+    mobileImgSrc: `${CDN_URL}/activities/activities-container__disco--desktop.jpg`
   },
   {
     altText: "Tea party for residents on Mother's Day",
-    desktopImgSrc: `${CDN_URL}/activities-container__tea--desktop.jpg`,
-    mobileImgSrc: `${CDN_URL}/activities-container__tea--desktop.jpg`
+    desktopImgSrc: `${CDN_URL}/activities/activities-container__tea--desktop.jpg`,
+    mobileImgSrc: `${CDN_URL}/activities/activities-container__tea--desktop.jpg`
   },
   {
     altText: "Santa visiting the residents during Christmas party",
-    desktopImgSrc: `${CDN_URL}/activities-container__santa--desktop.jpg`,
-    mobileImgSrc: `${CDN_URL}/activities-container__santa--desktop.jpg`
+    desktopImgSrc: `${CDN_URL}/activities/activities-container__santa--desktop.jpg`,
+    mobileImgSrc: `${CDN_URL}/activities/activities-container__santa--desktop.jpg`
   }
 ];
 
