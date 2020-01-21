52df7264cc6fcd3a0434d1589ff5637c8830b75c
Aaron Sisler <aaron.sisler@eandbsolutions.com>
2020-01-21 10:33:57 -0500

    Updating the cdn url logic to allow for folders

--- src/config/config.js
+++ src/config/config.js
@@ -1,6 +1,8 @@
-// CDN UrL
+// CDN URL
 export const CDN_URL =
   "https://cdn-e-and-b-solutions.s3.amazonaws.com/waterbrooke";
+
+export const getCdnUrl = (folder = "") => `${CDN_URL}${folder}`;
 
 // Contact Email
 export const CONTACT_EMAIL = "aaron.sisler@eandbsolutions.com";
--- src/content/team-members.js
+++ src/content/team-members.js
@@ -1,49 +1,50 @@
-import { CDN_URL } from "../config";
-const teamMembersCDNFolder = "leadership";
+import { getCdnUrl } from "../config";
+
+const CDN_URL = getCdnUrl("/leadership");
 
 const teamMembers = [
   {
-    imgSrc: `${CDN_URL}/${teamMembersCDNFolder}/jennifer_white.png`,
+    imgSrc: `${CDN_URL}/jennifer_white.png`,
     name: "Jennifer White",
     title: "Activity Director"
   },
   {
-    imgSrc: `${CDN_URL}/${teamMembersCDNFolder}/dana_rabon.png`,
+    imgSrc: `${CDN_URL}/dana_rabon.png`,
     name: "Dana Rabon",
     title: "RPh Executive Director"
   },
   {
-    imgSrc: `${CDN_URL}/${teamMembersCDNFolder}/adela_pena.png`,
+    imgSrc: `${CDN_URL}/adela_pena.png`,
     name: "Adela Pena",
     title: "Dietary & Environmental Service Director"
   },
   {
-    imgSrc: `${CDN_URL}/${teamMembersCDNFolder}/brenda_helms.png`,
+    imgSrc: `${CDN_URL}/brenda_helms.png`,
     name: "Brenda Helms",
     title: "Human Resource Director"
   },
   {
-    imgSrc: `${CDN_URL}/${teamMembersCDNFolder}/marie_bell.png`,
+    imgSrc: `${CDN_URL}/marie_bell.png`,
     name: "Marie Bell",
     title: "Transportation Director"
   },
   {
-    imgSrc: `${CDN_URL}/${teamMembersCDNFolder}/morticia_butcher.png`,
+    imgSrc: `${CDN_URL}/morticia_butcher.png`,
     name: "Morticia Butcher",
     title: "Resident Care Director"
   },
   {
-    imgSrc: `${CDN_URL}/${teamMembersCDNFolder}/rhonda_boyd_todd.png`,
+    imgSrc: `${CDN_URL}/rhonda_boyd_todd.png`,
     name: "Rhonda Boyd Todd",
     title: "Administrator"
   },
   {
-    imgSrc: `${CDN_URL}/${teamMembersCDNFolder}/yolanda_thatch.png`,
+    imgSrc: `${CDN_URL}/yolanda_thatch.png`,
     name: "Yolanda Thatch",
     title: "Special Care Unit Director"
   },
   {
-    imgSrc: `${CDN_URL}/${teamMembersCDNFolder}/audrey_clark.png`,
+    imgSrc: `${CDN_URL}/audrey_clark.png`,
     name: "Audrey Clark",
     title: "Admission/Marketing Director"
   }
