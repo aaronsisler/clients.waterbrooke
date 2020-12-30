9f83a1b84f18721b1a6315756ddfdf95af928bdf
Aaron Sisler <aaron.sisler@eandbsolutions.com>
2020-12-30 17:22:38 -0500

    Adding in the apply page and container

---
+++ src/containers/apply-container/apply-container.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import "./apply-container.scss";
+
+const ApplyContainer = () => (
+  <div className="apply-container">ApplyContainer</div>
+);
+
+export default ApplyContainer;
