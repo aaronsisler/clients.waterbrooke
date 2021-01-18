a8e16af82a2734878a7e68ba383246d84c187159
Aaron Sisler <aaron.sisler@eandbsolutions.com>
2021-01-18 16:42:12 -0500

    Adding in the CSS pieces

--- src/containers/image-submission-container/image-submission-container.jsx
+++ src/containers/image-submission-container/image-submission-container.jsx
@@ -6,6 +6,11 @@ import "./image-submission-container.scss";
 
 const ImageSubmissionContainer = () => (
   <div className="image-submission-container">
+    <p>
+      To complete the application process, please upload a valid form of
+      identification.
+    </p>
+    <p>Valid forms of ID include: Driver's License, Passport</p>
     <ImageSubmissionForm />
   </div>
 );
